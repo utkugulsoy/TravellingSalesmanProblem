07b94b3abd50f0ccf4542eb89f858887c55dfaa9
Utku Kaan Gülsoy <47040490+utkugulsoy@users.noreply.github.com>
2021-06-06 18:13:10 +0300

    Nearest Neighbor Algorithm implemented. Problems solved.

--- NearestNeighbor/src/NNmain.java
+++ NearestNeighbor/src/NNmain.java
@@ -2,14 +2,16 @@ import java.io.IOException;
 
 public class NNmain {
     public static void main(String[] args) throws IOException {
-      NN_Body body=new NN_Body();
+
+        for(int i=0; i<4663; i++) {
+            NN_Body body = new NN_Body();
 
 
-            body.NN_Calculator(1);
+            body.NN_Calculator(i);
 
             System.out.println(body.tourLength);
 
-
+        }
 
     }
 }
